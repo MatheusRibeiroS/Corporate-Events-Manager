a6037491969f9d696a38c69621b4d899ad13be43
MatheusRibeiroS <matheus.ribeiro.mrs762@gmail.com>
2024-08-12 23:22:18 -0300

    feat: add middlewares

--- auth-service/drizzle.config.ts
+++ auth-service/drizzle.config.ts
@@ -1,15 +1,18 @@
-import type { Config } from "drizzle-kit";
+import { defineConfig } from "drizzle-kit";
 import * as dotenv from "dotenv";
 
 dotenv.config({ path: ".env" });
 
 if (!process.env.DATABASE_URL) throw new Error("DATABASE_URL is not set");
 
-export default {
-  schema: "./src/lib/db/*", // any schema file inside the /db folder.
-  out: "./drizzle",
+export default defineConfig({
+  schema: "./src/db/schema.ts",
+  out: "./src/db/migrations",
   driver: "pg",
   dbCredentials: {
     connectionString: process.env.DATABASE_URL,
   },
-} satisfies Config;
+  dialect: "sqlite",
+  verbose: true,
+  strict: true,
+}) satisfies defineConfig;
