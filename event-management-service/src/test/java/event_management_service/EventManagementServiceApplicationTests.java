349b1bb7183c0e1b2b4ccec3e4b3ded52e3e5fed
MatheusRibeiroS <matheus.ribeiro.mrs762@gmail.com>
2024-09-07 00:44:02 -0300

    feat: add initial event management service structure

---
+++ event-management-service/src/test/java/event_management_service/EventManagementServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package event_management_service.event_management_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EventManagementServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
